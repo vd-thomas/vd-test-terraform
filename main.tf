f6024fb9476b9d09cbf1fb1a631988a02230fd23
vd-thomas <58597136+vd-thomas@users.noreply.github.com>
2020-12-07 11:05:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,47 @@
-/*
-* main.tf
-*/
-
 provider "aws" {
-  region = ""
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
 }
 
-provider "google" {}
+module "global" {
+  source               = "./global"
 
-module "static-website" {
-  source  = "GeminiWind/static-website/aws"
-  version = "1.0.0"
-  # Name of CNAME record.
-  cname = ""
-  # Your root domain, e.g: example.dev.
-  domain = ""
-  # Path to your static website
-  artifact_dir = ""
-  # Name of your app.
-  app = ""
+  aws_dns_zone         = "${var.aws_dns_zone}"
+  bastion_dns_subdomain = "${var.bastion_dns_subdomain}"
+  aws_az1              = "${var.aws_az1}"
+  aws_az2              = "${var.aws_az2}"
+  vpc_cidr             = "${var.vpc_cidr}"
+  public1_subnet_cidr  = "${var.public1_subnet_cidr}"
+  public2_subnet_cidr  = "${var.public2_subnet_cidr}"
+  private1_subnet_cidr = "${var.private1_subnet_cidr}"
+  private2_subnet_cidr = "${var.private2_subnet_cidr}"
+  sg_ssh_cidr          = "${var.sg_ssh_cidr}"
+  admin_ssh_public_key = "${var.admin_ssh_public_key}"
+}
+
+module "gitlab" {
+  source                       = "./gitlab"
+
+  aws_region                   = "${var.aws_region}"
+  aws_dns_zone                 = "${var.aws_dns_zone}"
+  gitlab_max                   = "${var.gitlab_max}"
+  gitlab_min                   = "${var.gitlab_min}"
+  gitlab_static_instances      = "${var.gitlab_static_instances}"
+  gitlab_desired               = "${var.gitlab_desired}"
+  gitlab_db_name               = "${var.gitlab_db_name}"
+  gitlab_db_username           = "${var.gitlab_db_username}"
+  gitlab_db_password           = "${var.gitlab_db_password}"
+  gitlab_root_password         = "${var.gitlab_root_password}"
+  gitlab_ci_registration_token = "${var.gitlab_ci_registration_token}"
+  public1_subnet_cidr          = "${var.public1_subnet_cidr}"
+  public2_subnet_cidr          = "${var.public2_subnet_cidr}"
+
+  admin_ssh_key = "${module.global.admin_ssh_key}"
+  vpc_id = "${module.global.vpc_id}"
+  dns_zone_id = "${module.global.dns_zone_id}"
+  dns_zone_name = "${module.global.dns_zone_name}"
+  sg_bastions_id = "${module.global.sg_bastions_id}"
+  private_subnet_ids = "${module.global.private_subnet_ids}"
+  public_subnet_ids = "${module.global.public_subnet_ids}"
 }
